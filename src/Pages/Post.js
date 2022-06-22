0434736e6ffdb38fc434fb67373fb48e2a6cf5f3
Kamrul Hasan <kamrulsdev@gmail.com>
2022-06-22 07:40:31 +0600

    Ui design developed and data loaded form the api on UI completed

---
+++ src/Pages/Post.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import avatar from '../images/profileAvater.png'
+import { BsThreeDots } from 'react-icons/bs';
+import { GrLike } from 'react-icons/gr';
+import { FaRegComments } from 'react-icons/fa';
+import { FaShareSquare } from 'react-icons/fa';
+// import postImage from '../images/postimage.png'
+
+const Post = ({ postContent }) => {
+    const { created_at, user, profile_images, post_contents, post_details, liked_posts_count, comments_count } = postContent;
+
+    console.log(post_contents[0]?.source);
+    console.log(post_contents[0]?.content_name);
+
+
+    /*---------------Profile Photo Location + friend_profile_photo--------------*/
+    const profileImg = `https://uviom-life.s3.amazonaws.com/images/personal/friend_profile_photo/${profile_images.friend_profile_photo}`;
+    console.log('prifle img', profileImg);
+
+    /*--------------Post Image Location + content_name------------*/
+    const postImg = `https://uviom-life.s3.amazonaws.com/images/content/post_images/${post_contents[0].content_name}`;
+    console.log('post img', postImg);
+
+    return (
+        <div className='p-4 bg-[#F1F1F1] rounded'>
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img src={profileImg} className="w-14 border-gray-600 border-2 rounded-full" alt="" />
+                    <div className='ml-4'>
+                        <h4>{user.first_name} ({user.last_name})</h4>
+                        <h5>{created_at}1 hr ago</h5>
+                    </div>
+                </div>
+                <div className='cursor-pointer'>
+                    <BsThreeDots size={24} />
+                </div>
+            </div>
+            <div className='py-4'>
+                <img src={postImg} alt="" />
+                <p className='p-3'>{post_details}</p>
+            </div>
+            <div className='flex justify-between px-12'>
+                <div className='flex justify-center items-center bg-white px-3 py-1 rounded-full cursor-pointer hover:bg-blue-100'>
+                    <GrLike /> <span className='ml-1'>{liked_posts_count} Like</span>
+                </div>
+                <div className='flex justify-center items-center bg-white px-3 py-1 rounded-full cursor-pointer hover:bg-blue-100'>
+                    <FaRegComments /> <span className='ml-1'>{comments_count} Comment</span>
+                </div>
+                <div className='flex justify-center items-center bg-white px-3 py-1 rounded-full cursor-pointer hover:bg-blue-100'>
+                    <FaShareSquare /><span className='ml-1'>Share</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Post;
---
+++ src/Pages/Posts.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import Post from './Post';
+
+const Posts = () => {
+    const [postContents, setPostContents] = useState([]);
+    console.log(postContents);
+
+    useEffect(() => {
+        const postData = async () => {
+            await fetch('https://backend.uviom.com/frontend_api/test-data')
+                .then(res => res.json())
+                .then(data => setPostContents(data.data))
+        }
+        postData();
+    }, []);
+    return (
+        <section className='w-[600px] w- mx-auto mt-12 px-10'>
+            <h2 className='my-2'>post</h2>
+            <div>
+                <div className='grid lg:grid-cols-1 gap-5'>
+                    {
+                        postContents.map(postContent => <Post
+                            key={postContent.id}
+                            postContent={postContent}
+                        ></Post>)
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Posts; <h2>This is posts page</h2>
