58a569cd2b4d4462a346c1874fd92d06bd979699
Kamrul Hasan <kamrulsdev@gmail.com>
2022-06-23 08:40:48 +0600

    UI designed for pagination

--- src/Pages/Posts.js
+++ src/Pages/Posts.js
@@ -6,13 +6,15 @@ import Loading from './Loading';
 const Posts = () => {
     const [postContents, setPostContents] = useState([]);
     const [hasMore, sethasMore] = useState(true);
-    const [page, setPage] = useState(2);
+    const [page, setPage] = useState(1);
 
+    const [pageCount, setPageCount] = useState(4);
+    // const [size, setSize] = useState(10);
 
 
     useEffect(() => {
         const postData = async () => {
-            await fetch(`https://backend.uviom.com/public/frontend_api/test-data?page=1&per_page=7`)
+            await fetch(`https://backend.uviom.com/public/frontend_api/test-data`)
                 .then(res => res.json())
                 .then(data => setPostContents(data.data))
         }
@@ -40,6 +42,17 @@ const Posts = () => {
     return (
         <>
             <section className='w-[600px] w- mx-auto mt-12 px-10 mb-10'>
+                <div className='text-center'>
+                    <button className='mr-3 hover:text-blue-500'>{"<<"}Prev</button>
+                    {
+                        [...Array(pageCount).keys()].map((number, index) => <button
+                            onClick={() => setPage(number)}
+                            key={index} className='btn w-6 rounded bg-slate-400 hover:bg-blue-100 mr-3'>
+                            {number + 1}
+                        </button>)
+                    }
+                    <button className='hover:text-blue-500'>Next{">>"}</button>
+                </div>
                 <h2 className='my-2'>post {postContents.length}</h2>
                 <div>
                     <InfiniteScroll
